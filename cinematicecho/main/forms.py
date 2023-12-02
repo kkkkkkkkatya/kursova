5887d4a7e94361d8dd6705ed2e3864f982e1399a
kkkkkkkkatya <vasyabarilyuk@gmail.com>
2023-12-02 15:15:18 +0200

    main draft done

--- cinematicecho/main/forms.py
+++ cinematicecho/main/forms.py
@@ -1,18 +1,27 @@
 from .models import Films
-from django.forms import ModelForm, TextInput, Textarea
+from django.forms import ModelForm, TextInput, Textarea, Select
 
 
 class FilmsForm(ModelForm):
     class Meta:
         model = Films
-        fields = ["name", "description"]
+        year_choices = [(year, int(year)) for year in range(1900, 2024)]
+        fields = ["name", "year", "director", "description"]
         widgets = {
             'name' : TextInput(attrs={
-            'placeholder' : "Челюсті 3",
+            'placeholder' : "Сам удома",
+            'class' : "form-control"
+        }),
+            'year' : Select(choices=year_choices, attrs={
+            'placeholder' : "1990",
+            'class' : "form-control"
+        }),
+            'director' : TextInput(attrs={
+            'placeholder' : "Кріс Коламбус",
             'class' : "form-control"
         }),
             'description': Textarea(attrs={
-                'placeholder': "У далекому 2005 році було знято...",
+                'placeholder': "Восьмирічний Кевін МакКалістер мріє лише про одне...",
                 'class': "form-control"
             })
         }
--- cinematicecho/main/views.py
+++ cinematicecho/main/views.py
@@ -1,17 +1,29 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Films
 from .forms import FilmsForm
+from .keeper_service import keeper_service
 
 
-# Create your views here.
+# Create your CONTROLLERS here.
+
+### film list for users ###
 def index(request):
-    film_list = Films.objects.all()
+    film_list = Films.objects.order_by('-id')
     return render(request, 'main/index.html', {'film_list':film_list})
 
+### about site ###
 def about(request):
     return render(request, 'main/about.html')
 
+### film list for admins ###
+def list(request):
+    error = keeper_service.pop("error")
+    film_list = Films.objects.order_by('-id')
+    return render(request, 'main/list.html', {'film_list':film_list, 'error':error})
+
+### create film ###
 def create(request):
+    print(f"=== main, action: create === ")
     error= ''
     if request.method == "POST":
         form = FilmsForm(request.POST)
@@ -27,3 +39,56 @@ def create(request):
         'error' : error
     }
     return render(request, 'main/create.html', context)
+
+### edit film ###
+def edit(request, id):
+    print(f"=== main, action: edit === ")
+    print(f"id: ", id)
+    error = ''
+
+    film = get_object_or_404(Films, id=id)
+
+    ### update film
+    if request.method == 'POST':
+        form = FilmsForm(request.POST, instance=film)
+        if form.is_valid():
+            form.save()
+            return redirect('show', id=film.id)  # Redirect to film detail view
+        else:
+            error = 'Помилки при заповненні форми'
+
+    ### open form for updating
+    else:
+        form = FilmsForm(instance=film)
+
+    return render(request, 'main/edit.html', {'form': form, 'error':error})
+
+
+
+### delete film ###
+def delete(request, id):
+    print(f"=== main, action: delete === ")
+    print(f"id: ", id)
+
+    try:
+        obj = None
+        obj = Films.objects.get(id=id)
+        print(f"obj: {obj}")
+        obj.delete()
+    except (Exception) as e:
+        keeper_service.push("error", str(e))
+        print(f"Error: {e}")
+
+    return redirect('list')
+
+
+### show film ###
+def show(request, id):
+    print(f"=== main, action: show === ")
+    print(f"id: ", id)
+    error = ''
+
+    film = get_object_or_404(Films, id=id)
+    form = FilmsForm(instance=film)
+
+    return render(request, 'main/show.html', {'form': form, 'error':error, 'id':id})
